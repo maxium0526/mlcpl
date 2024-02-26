127592dcdf1206b28ea1fbe2cf17eaa75d07085d
maxium0526 <38188772+maxium0526@users.noreply.github.com>
2024-02-26 21:21:40 +0800

    add unknown_as

--- mlcpl/sample_mix.py
+++ mlcpl/sample_mix.py
@@ -38,10 +38,11 @@ class LogicMix(torch.utils.data.Dataset):
         return image, target
     
 class MixUp(torch.utils.data.Dataset):
-    def __init__(self, dataset, alpha=0.2, transform=None):
+    def __init__(self, dataset, alpha=0.2, transform=None, unknown_as=None):
         self.dataset = dataset
         self.alpha = alpha
         self.transform = transform
+        self.unknown_as = unknown_as
         self.num_categories = self.dataset.num_categories
 
     def __len__(self):
@@ -51,6 +52,10 @@ class MixUp(torch.utils.data.Dataset):
 
         img1, target1 = self.dataset[idx]
         img2, target2 = self.dataset[np.random.randint(0, len(self.dataset))]
+
+        if self.unknown_as:
+            target1 = torch.where(torch.isnan(target1), self.unknown_as, target1)
+            target2 = torch.where(torch.isnan(target2), self.unknown_as, target2)
 
         if torch.isnan(target1).any() or torch.isnan(target2).any():
             raise Exception('Target contains nan.')
