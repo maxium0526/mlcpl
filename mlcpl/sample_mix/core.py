e3bd5c21dd484b74212dbd164a8cecfd52781bea
maxium0526 <38188772+maxium0526@users.noreply.github.com>
2024-03-03 17:47:35 +0800

    add estimate_target_mix_strategy

--- mlcpl/sample_mix/core.py
+++ mlcpl/sample_mix/core.py
@@ -1,4 +1,5 @@
 import torch
+from ..dataset import *
 
 def mixup(tensor_1, tensor_2, lam=0.5):
     return lam * tensor_1 + (1 - lam) * tensor_2
@@ -36,3 +37,66 @@ def mix_targets(targets, strict_negative=False):
         new_target = torch.where(new_target == -1, 0, new_target)
 
         return new_target
+
+class MixTargets():
+    def __init__(self, strict_negative=False) -> None:
+        self.strict_negative = strict_negative
+
+    def __call__(self, targets):
+        return mix_targets(targets, strict_negative=self.strict_negative)
+
+def estimate_target_mix_strategy(strategy, partial_dataset, full_dataset, strict_negative):
+    if len(partial_dataset) != len(full_dataset):
+        raise Exception('The datasets have different sizes.')
+    
+    num_samples = len(partial_dataset)
+    num_categories = partial_dataset.num_categories
+
+    total_tp, total_tn, total_fp, total_fn = 0, 0, 0, 0
+    records = []
+    for i in range(num_samples):
+        print(f'{i}/{num_samples}; tp={total_tp}, tn={total_tn}, fp={total_fp}, fn={total_fn}', end='\r')
+        random_index = np.random.randint(0, num_samples)
+
+        partial_records = [partial_dataset.records[a] for a in [i, random_index]]
+        partial_targets = [labels_to_one_hot(p, n, u, num_categories) for _, _, p, n, u in partial_records]
+
+        full_records = [full_dataset.records[a] for a in [i, random_index]]
+        full_targets = [labels_to_one_hot(p, n, u, num_categories) for _, _, p, n, u in full_records]
+
+        partial_new_target = strategy(partial_targets)
+        full_new_target = mix_targets(full_targets, strict_negative=True)
+
+        tp, tn, fp, fn = 0, 0, 0, 0
+        for partial_target, full_target in zip(partial_new_target, full_new_target):
+            if torch.isnan(partial_target):
+                continue
+
+            if partial_target == 1:
+                if full_target == 1:
+                    tp += 1
+                elif full_target == 0:
+                    fp += 1
+                else:
+                    raise Exception(f'Full target = {full_target}. Neither 0 nor 1.')
+            elif partial_target == 0:
+                if full_target == 1:
+                    fn += 1
+                elif full_target == 0:
+                    tn += 1
+                else:
+                    raise Exception(f'Full target = {full_target}. Neither 0 nor 1.')
+            else:
+                raise Exception(f'Full target = {partial_target}. Neither 0 nor 1 nor nan.')
+            
+        total_tp += tp
+        total_tn += tn
+        total_fp += fp
+        total_fn += fn
+
+        record = (tp, tn, fp, fn)
+        records.append(record)
+            
+    print()
+
+    pd.DataFrame(records, columns=['TP', 'TN', 'FP', 'FN']).to_csv('false_labels.csv')
