b512761ae1a9944da26bf73b6cd2371fdd507672
maxium0526 <38188772+maxium0526@users.noreply.github.com>
2024-04-08 18:03:26 +0800

    change ml-mixup to uniform distribution

--- mlcpl/sample_mix/multilabel_mixup.py
+++ mlcpl/sample_mix/multilabel_mixup.py
@@ -3,9 +3,8 @@ import numpy as np
 from .core import *
 
 class MultilabelMixUp(torch.utils.data.Dataset):
-    def __init__(self, dataset, alpha=0.2, transform=None, unknown_as=0):
+    def __init__(self, dataset, transform=None, unknown_as=0):
         self.dataset = dataset
-        self.alpha = alpha
         self.transform = transform
         self.unknown_as = unknown_as
         self.num_categories = self.dataset.num_categories
@@ -18,7 +17,7 @@ class MultilabelMixUp(torch.utils.data.Dataset):
         img1, target1 = self.dataset[idx]
         img2, target2 = self.dataset[np.random.randint(0, len(self.dataset))]
 
-        lam = np.random.beta(self.alpha, self.alpha)
+        lam = np.random.uniform()
 
         img = mixup(img1, img2, lam=lam)
         target = logic_mix_targets([target1, target2], strict_negative=True, unknown_as=self.unknown_as)
