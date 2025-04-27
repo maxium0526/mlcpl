2bf9c27ead832c73a025be9c1e9d58b936faf30d
Max Chong <38188772+maxium0526@users.noreply.github.com>
2025-04-27 23:49:59 +0100

    polish datasets

--- src/mlcpl/dataset/core.py
+++ src/mlcpl/dataset/core.py
@@ -1,4 +1,5 @@
 import torch
+from torch import Tensor
 from torch.utils.data import Dataset
 import numpy as np
 import os
@@ -7,7 +8,7 @@ from PIL import Image
 import json
 from ..helper import dotdict
 import copy
-from typing import List, Callable
+from typing import List, Callable, Tuple, Dict
 
 def read_jpg(img_path):
     return Image.open(img_path).convert('RGB')
@@ -16,7 +17,7 @@ class MLCPLDataset(Dataset):
     def __init__(self,
                  name: str,
                  dataset_path: str,
-                 records: List[tuple],
+                 records: List[Tuple],
                  num_categories: int,
                  transform: Callable,
                  categories: List[str] = None,
@@ -56,32 +57,62 @@ class MLCPLDataset(Dataset):
             f'# # of Samples: {len(self)}\n' \
             f'# # of Categories: {self.num_categories}\n' \
             f'# Label Proportion: {statistics["label_ratio"]*100:.2f}%\n' \
-            f'# Positive-Negative Imbalance: {statistics["num_positive_labels"] / (statistics["num_positive_labels"] + statistics["num_negative_labels"])*100:.2f}%\n' \
+            f'# Positive-Negative Balance: {statistics["num_positive_labels"] / statistics["num_known_labels"]*100:.2f} : {statistics["num_negative_labels"] / statistics["num_known_labels"]*100:.2f}%\n' \
             f'{"-"*40}\n'
         
         print(info)
 
 
     def __getitem__(self, idx):
-        id, path, pos_category_nos, neg_category_nos = self.records[idx]
+        """loads and returns a sample from the dataset at the given index idx. See https://pytorch.org/tutorials/beginner/basics/data_tutorial.html.
+        """
+
+        id, path, pos_categories, neg_categories = self.records[idx]
         img_path = os.path.join(self.dataset_path, path)
         img = self.read_func(img_path)
         img = self.transform(img)
-        target = self.__to_one_hot(pos_category_nos, neg_category_nos)
+        target = self.__to_one_hot(pos_categories, neg_categories)
         return img, target
     
-    def __to_one_hot(self, pos_category_nos, neg_category_nos):
-        return labels_to_one_hot(pos_category_nos, neg_category_nos, self.num_categories)
+    def __to_one_hot(self, pos_categories: List[int], neg_categories: List[int]) -> Tensor:
+        """Produce an one-hot vector based on given categories.
+
+        Args:
+            pos_categories (List[int]): Positive categories
+            neg_categories (List[int]): Negative categories
+
+        Returns:
+            Tensor: An one-hot tensor with shape (N).
+        """
+        return labels_to_one_hot(pos_categories, neg_categories, self.num_categories)
     
     def get_statistics(self):
+        """Return some statistics given the dataset's informations
+
+        Args:
+            records (List[Tuple]): In consists of information of samples. Each tuple store a sample's (id, img_path, list of positive categories, list of negative categories)
+            num_categories (int): The total number of categories
+
+        Returns:
+            Dict: A dict consisting of statistics
+        """
         return get_statistics(self.records, self.num_categories)
     
-    def drop_labels_random(self, target_partial_ratio, seed=526):
-        self.records = drop_labels(self.records, target_partial_ratio, seed=seed)
+    def drop_labels_uniform(self, target_label_proportion: float, seed: int = 526):
+        self.records = drop_labels_uniform(self.records, target_label_proportion, seed=seed)
         return self
     
-    def drop_labels_fix_per_category(self, max_num_labels_per_category, seed=526):
-        # https://openaccess.thecvf.com/content/CVPR2022/papers/Ben-Baruch_Multi-Label_Classification_With_Partial_Annotations_Using_Class-Aware_Selective_Loss_CVPR_2022_paper.pdf
+    def drop_labels_fix_per_category(self, max_num_labels_per_category: int, seed: int = 526):
+        """Drop labels with the FPC method. See https://openaccess.thecvf.com/content/CVPR2022/papers/Ben-Baruch_Multi-Label_Classification_With_Partial_Annotations_Using_Class-Aware_Selective_Loss_CVPR_2022_paper.pdf
+
+        Args:
+            max_num_labels_per_category (int): The maximum number of labels for each category
+            seed (int, optional): The random seed. Defaults to 526.
+
+        Returns:
+            Self: self
+        """
+
         rng = np.random.Generator(np.random.PCG64(seed=seed))
 
         per_category_positive_samples = []
@@ -90,7 +121,7 @@ class MLCPLDataset(Dataset):
             per_category_positive_samples.append([])
             per_category_negative_samples.append([])
 
-        for i, path, positives, negatives, uncertains in self.records:
+        for i, path, positives, negatives in self.records:
             for positive in positives:
                 per_category_positive_samples[positive].append(i)
             for negative in negatives:
@@ -108,15 +139,15 @@ class MLCPLDataset(Dataset):
             per_category_negative_samples[c] = per_category_negative_samples[c][:N_s]
 
         new_records = []
-        for i, path, _, _, _ in self.records:
+        for i, path, _, _ in self.records:
             positives = [c for c in range(self.num_categories) if i in per_category_positive_samples[c]]
             negatives = [c for c in range(self.num_categories) if i in per_category_negative_samples[c]]
-            new_records.append((i, path, positives, negatives, []))
+            new_records.append((i, path, positives, negatives))
         
         self.records = new_records
         return self
     
-    def drop_labels_natural(self, N, alpha=1, beta=5, seed=526):
+    def __drop_labels_natural(self, N, alpha=1, beta=5, seed=526): # deprecated
         rng = np.random.Generator(np.random.PCG64(seed=seed))
 
         new_records = []
@@ -130,24 +161,54 @@ class MLCPLDataset(Dataset):
             new_num_positives = np.min([round(rng.beta(alpha, beta) * N), num_positives])
             new_num_negatives = np.min([round(rng.beta(alpha, beta) * N), num_negatives])
 
-            new_records.append((id, path, positives[:new_num_positives], negatives[:new_num_negatives], uncertains))
+            new_records.append((id, path, positives[:new_num_positives], negatives[:new_num_negatives]))
         
         self.records = new_records
 
         return self
 
-def labels_to_one_hot(positives, negatives, num_categories):
+def labels_to_one_hot(positives: List[int], negatives: List[int], num_categories: int) -> Tensor:
+    """Produce an one-hot vector based on given categories.
+
+    Args:
+        positives (List[int]): Positive categories
+        negatives (List[int]): Negative categories
+        num_categories (int): The number of categories (N).
+
+    Returns:
+        Tensor: An one-hot tensor with shape (N).
+    """
+
     one_hot = torch.full((num_categories, ), torch.nan, dtype=torch.float32)
     one_hot[np.array(positives)] = 1.0
     one_hot[np.array(negatives)] = 0.0
     return one_hot
 
-def one_hot_to_labels(one_hot):
+def one_hot_to_labels(one_hot: Tensor) -> Tuple[List[int], List[int]]:
+    """Inverse function of labels_to_one_hot
+
+    Args:
+        one_hot (Tensor): An one-hot tensor with shape (N)
+
+    Returns:
+        Tuple[List[int], List[int]]: Two lists containing positive categories and negative categories, respectively.
+    """
+
     positives = (one_hot ==1).nonzero().flatten().tolist()
     negatives = (one_hot ==0).nonzero().flatten().tolist()
     return positives, negatives
     
-def get_statistics(records, num_categories):
+def get_statistics(records: List[Tuple], num_categories: int) -> Dict:
+    """Return some statistics given the dataset's informations
+
+    Args:
+        records (List[Tuple]): In consists of information of samples. Each tuple store a sample's (id, img_path, list of positive categories, list of negative categories)
+        num_categories (int): The total number of categories
+
+    Returns:
+        Dict: A dict consisting of statistics
+    """
+
     num_categories = num_categories
     num_samples = len(records)
     num_labels = num_samples * num_categories
@@ -158,8 +219,6 @@ def get_statistics(records, num_categories):
     for i, (_, _, positive_labels, negative_labels) in enumerate(records):
         all_positive_labels += positive_labels
         all_negative_labels += negative_labels
-    
-    print()
 
     all_positive_labels = pd.Series(all_positive_labels)
     all_negative_labels = pd.Series(all_negative_labels)
@@ -205,43 +264,92 @@ def get_statistics(records, num_categories):
 
     return statistics
 
-def fill_nan_to_negative(old_records, num_categories):
+def fill_nan_to_negative(old_records: List[Tuple], num_categories: int) -> List[Tuple]:
+    """ Put categories that are implicitly negative (i.e., neither positive nor negaitve) into negative
+
+    Args:
+        old_records (List[Tuple]): Original records
+        num_categories (int): The total number of categories
+
+    Returns:
+        List[Tuple]: New records
+    """
     new_records = []
-    for (i, path, pos_category_nos, neg_category_nos) in old_records:
-        new_neg_category_nos = [x for x in range(num_categories) if x not in pos_category_nos]
-        new_records.append((i, path, pos_category_nos, new_neg_category_nos))
+    for (i, path, pos_categories, neg_categories) in old_records:
+        new_neg_categories = [x for x in range(num_categories) if x not in pos_categories]
+        new_records.append((i, path, pos_categories, new_neg_categories))
     return new_records
 
-def drop_labels(old_records, target_partial_ratio, seed=526):
+def drop_labels_uniform(old_records: List[Tuple], target_label_proportion: float, seed: int = 526) -> List[Tuple]:
+    """Randomly drop labels that each label has the same probability to be dropped.
+
+    Args:
+        old_records (List[Tuple]): Original records.
+        target_label_proportion (float): Desired label proportion of the new records.
+        seed (int, optional): The random seed. Defaults to 526.
+
+    Returns:
+        List[Tuple]: New records.
+    """
+
     rng = np.random.Generator(np.random.PCG64(seed=seed))
 
     new_records = []
-    for (i, path, pos_category_nos, neg_category_nos) in old_records:
-        new_pos_category_nos = [no for no in pos_category_nos if rng.random() < target_partial_ratio]
-        new_neg_category_nos = [no for no in neg_category_nos if rng.random() < target_partial_ratio]
-        new_records.append((i, path, new_pos_category_nos, new_neg_category_nos))
+    for (i, path, pos_categories, neg_categories) in old_records:
+        new_pos_categories = [no for no in pos_categories if rng.random() < target_label_proportion]
+        new_neg_categories = [no for no in neg_categories if rng.random() < target_label_proportion]
+        new_records.append((i, path, new_pos_categories, new_neg_categories))
     return new_records
 
-def records_to_df(records):
+def records_to_df(records: List[Tuple]) -> pd.DataFrame:
+    """Convert records to a dataframe
+
+    Args:
+        records (List[Tuple]): Records
+
+    Returns:
+        pd.DataFrame: A dataframe
+    """
+
     df = pd.DataFrame(records, columns=['Id', 'Path', 'Positive', 'Negative'])
     return df
 
-def df_to_records(df):
+def df_to_records(df: pd.DataFrame) -> List[Tuple]:
+    """Convert a dataframe to records
+
+    Args:
+        df (pd.DataFrame): A dataframe
+
+    Returns:
+        List[Tuple]: records
+    """
     records = []
     for i, row in df.iterrows():
         id = row['Id']
         path = row['Path']
-        pos_category_nos = json.loads(row['Positive'])
-        neg_category_nos = json.loads(row['Negative'])
-        records.append((id, path, pos_category_nos, neg_category_nos))
+        pos_categories = json.loads(row['Positive'])
+        neg_categories = json.loads(row['Negative'])
+        records.append((id, path, pos_categories, neg_categories))
     
     return records
 
-def divide(dataset, divide_ratio=0.5, shuffle=True, seed=526):
+def split_dataset(dataset: MLCPLDataset, split_ratio: float =0.5, shuffle: bool = True, seed: int = 526) -> Tuple[MLCPLDataset, MLCPLDataset]:
+    """Split a MLCPLDataset into two MLCPLDatasets
+
+    Args:
+        dataset (MLCPLDataset): The dataset to be divied
+        divide_ratio (float, optional): Ratio of split. Defaults to 0.5.
+        shuffle (bool, optional): Whether shuffling the samples before split. Defaults to True.
+        seed (int, optional): The random seed. Defaults to 526.
+
+    Returns:
+        Tuple[MLCPLDataset, MLCPLDataset]: The two datasets
+    """
+
     rng = np.random.Generator(np.random.PCG64(seed=seed))
     records = copy.deepcopy(dataset.records)
     rng.shuffle(records)
-    split_at = round(len(records)*divide_ratio)
+    split_at = round(len(records)*split_ratio)
     records_1 = records[:split_at]
     records_2 = records[split_at:]
 
