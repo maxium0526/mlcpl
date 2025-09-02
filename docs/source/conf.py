bcaacdd07153e42fec7e968217232e38490a5d52
Max Chong <38188772+maxium0526@users.noreply.github.com>
2025-09-02 20:18:52 +0800

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,3 +27,8 @@ exclude_patterns = []
 
 html_theme = 'press'
 html_static_path = ['_static']
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..')) # Adjust path as needed
+sys.path.insert(0, os.path.abspath('..source')) # Adjust path as needed
