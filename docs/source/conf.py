fed458ce3251cebba21c389381b962305c92bbbe
Max Chong <38188772+maxium0526@users.noreply.github.com>
2025-09-02 20:23:32 +0800

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,3 +32,4 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..')) # Adjust path as needed
 sys.path.insert(0, os.path.abspath('..src')) # Adjust path as needed
+sys.path.insert(0, os.path.abspath('src')) # Adjust path as needed
