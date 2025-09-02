2943b2ad3969821ab9086c624ca4ad8acec16f26
Max Chong <38188772+maxium0526@users.noreply.github.com>
2025-09-02 20:29:41 +0800

    f

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,5 +31,5 @@ html_static_path = ['_static']
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..')) # Adjust path as needed
-sys.path.insert(0, os.path.abspath('..src')) # Adjust path as needed
-sys.path.insert(0, os.path.abspath('src')) # Adjust path as needed
+sys.path.insert(0, os.path.abspath('../src')) # Adjust path as needed
+sys.path.insert(0, os.path.abspath('./src')) # Adjust path as needed
